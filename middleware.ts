4aa6984fc77436e913d469d3684edd78a407dc26
nsdavian <nsdavenorth@gmail.com>
2024-05-04 15:17:37 +0100

    bajon0.2

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // export default clerkMiddleware();
 
-const isProtectedRoute = createRouteMatcher([
+const isProtectedRoute = createRouteMatcher([ 
     '/'
     // '/forum(.*)',
   ]);
   
   export default clerkMiddleware((auth, req) => {
     if (isProtectedRoute(req)) auth().protect();
+
+    publicRoutes: ['/api/webhooks/clerk']
   });
 
 export const config = {
